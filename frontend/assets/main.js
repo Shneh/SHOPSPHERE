516f052aef7a9244ba705ed73da177435921e9b7
Shneh Rawat <shnehrawat@Shnehs-MacBook-Air.local>
2025-05-28 14:14:58 +0530

    🔐 Added Firebase Google login with working popup and logout

--- frontend/assets/main.js
+++ frontend/assets/main.js
@@ -319,15 +319,6 @@ function checkout() {
   });
 }
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyD8ZLuzCYOkg8Yfny1NfhaGzOb4--11VjA",
-    authDomain: "shopsphere-auth.firebaseapp.com",
-    projectId: "shopsphere-auth",
-    storageBucket: "shopsphere-auth.appspot.com",
-    messagingSenderId: "684046783153",
-    appId: "1:684046783153:web:5b8f11a17e815719cf4890",
-    measurementId: "G-P3F4K7PXTD"
-  };
 
   firebase.initializeApp(firebaseConfig);
   function loginWithGoogle() {
